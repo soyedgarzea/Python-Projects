4fa8b80b265ae0b7808303fe400865705e919637
Edgar Zea <cesszea@icloud.com>
2024-09-10 15:55:11 -0600

    feat(python): handle routes

---
+++ server/README.md
@@ -0,0 +1,11 @@
+Start server from root
+
+1- Activate venv
+```
+$ . .venv/bin/activate
+```
+
+2- run app
+```
+flask --app server/server run --debug -p 3000
+```
--- server/server.py
+++ server/server.py
@@ -1,9 +1,23 @@
+import csv
 from typing import Any
-from flask import Flask, render_template # type: ignore
+from flask import Flask, redirect, render_template, request # type: ignore
 
 app = Flask(__name__) # type: ignore
 
-print(__name__)
+def valid_login(username: str, password: str): # type: ignore
+    '''
+    valid_login
+    '''
+    return True
+
+def write_to_csv(username: str, password: str):
+    '''
+    write_to_csv
+    '''
+    print(username, password)
+    with open('./server/data.csv', mode='a', newline='') as database:
+        writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([username, password])
 
 @app.route("/") # type: ignore
 def hello_world() -> Any:
@@ -13,6 +27,7 @@ def hello_world() -> Any:
     return render_template('index.html') # type: ignore
     # return "<p>Hello, World!</p>"
 
+# Path Params
 @app.route("/profile/<name>") # type: ignore
 def blog(name: str) -> Any:
     '''
@@ -28,3 +43,25 @@ def page(page_name: str) -> Any:
     '''
     return render_template(page_name)  # type: ignore
     # return "<p>Hello, World!</p>"
+
+# Request Object
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    '''
+    login
+    '''
+    error = None
+
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        username = data['username']
+        password = data['password']
+
+        if valid_login(username, password):
+            write_to_csv(username, password)
+            return redirect('/home.html')
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
